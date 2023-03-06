069e1423a4a4937154eadc15d819ceb6534a2d65
KyoungOh_0329 <mazoola12@gmail.com>
2023-03-06 20:53:50 +0900

    first commit

---
+++ 1109.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	char name[30];
+	int age;
+	char code;
+	double key;
+	scanf("%s %d %c %lf",&name,&age,&code,&key);
+	
+	printf("%s\n%d\n%c\n%g",name,age,code,key);
+}
